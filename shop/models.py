c48d589dc3b4980603cc44580bfdd26e7dded178
Ilya <ilusanik16@gmail.com>
2026-02-21 09:31:29 +0300

    feat: добавлены модели товара, категории, производителя и корзины с валидацией, сделаны мирации

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    product_image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    def clean(self):
+        if self.price < 0:
+            raise ValidationError("Price cannot be negative")
+
+        if self.stock_quantity < 0:
+            raise ValidationError("Stock quantity cannot be negative")
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="cart"
+    )
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+    def total_price(self):
+        return sum(item.total_price() for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField()
+
+    def clean(self):
+        if self.quantity > self.product.stock_quantity:
+            raise ValidationError(
+                f"Only {self.product.stock_quantity} items available in stock"
+            )
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity} pcs)"
